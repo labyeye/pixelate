1ec98d69c5dd0c75302eac002c503c24445d3cbc
Labh Bothra <labhbothra1380@gmail.com>
2025-05-06 04:44:24 +0530

    CRM DONE

--- mern-contact-dashboard/routes/authRoutes.js
+++ mern-contact-dashboard/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/authmiddleware');
 
 // Admin login
 // authRoutes.js
